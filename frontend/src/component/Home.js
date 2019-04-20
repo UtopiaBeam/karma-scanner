fb9d1f0cc257d0dbc4121e28d7e1619c5443e654
UtopiaBeam <utopiabeam@gmail.com>
2019-04-20 19:47:32 +0700

    Add result rendering

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,29 +1,98 @@
-import React from "react";
-import { Link } from "react-router-dom"
-import "./Home.css";
-import bg from "../img/bg-buddha.png";
+import React from 'react';
+import './Home.css';
+import bg from '../img/bg-buddha.png';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="component-home row">
-                <img className="background col m-0 p-0" src={bg} alt="Buddha looking judgmeentally" />
-                <div className="form text-center">
-                    <h1 className="row">วัดผลกรรม</h1>
-                    <div classNae="row">
-                        <form className="col">
-                            <p className="row">ชื่อ-นามสกุล</p>
-                            <input type="text" className="row"/>
-                            <p className="row">ชาตกรรม</p>
-                            <input type="date" className="row"/>
-                        </form>
-                    </div>
-                    <button className="btn row">
-                        <Link to="/result">รับผลกรรม</Link>
-                    </button>
-                </div>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      birthday: undefined,
+      firstName: undefined,
+      lastName: undefined,
+      redirect: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async onFormSubmit(event) {
+    event.preventDefault();
+    const { data } = await axios({
+      method: 'post',
+      url: 'https://us-central1-karmascanner.cloudfunctions.net/karma',
+      data: this.state,
+    });
+    this.setState({
+      deathday: data.deathday,
+      redirect: true,
+    });
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/result',
+            state: {
+              deathday: this.state.deathday,
+            },
+          }}
+        />
+      );
     }
+    return (
+      <div className="component-home row">
+        <img
+          className="background col m-0 p-0"
+          src={bg}
+          alt="Buddha looking judgmentally"
+        />
+        <div className="form text-center">
+          <h1 className="row">วัดผลกรรม</h1>
+          <div className="row">
+            <form className="col" onSubmit={this.onFormSubmit}>
+              <p className="row">ชื่อ</p>
+              <input
+                type="text"
+                name="firstName"
+                className="row"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+              <p className="row">นามสกุล</p>
+              <input
+                type="text"
+                name="lastName"
+                className="row"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+              <p className="row">ชาตกรรม</p>
+              <input
+                type="date"
+                name="birthday"
+                className="row"
+                value={this.state.birthday}
+                onChange={this.handleChange}
+              />
+              <button type="submit" className="row">
+                รับผลกรรม
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Home;
--- frontend/src/component/Result.js
+++ frontend/src/component/Result.js
@@ -1,13 +1,29 @@
-import React from "react";
-import "./Result.css";
+import React from 'react';
+import './Result.css';
 
 class Result extends React.Component {
-    render() {
-        return (
-            <div className="component-result">
-                <p className="title">ผลกรรม</p>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = props.location.state;
+    console.log(this.state);
+  }
+  getDateString() {
+    const date = new Date(this.state.deathday);
+    const res = date.toLocaleString('th-TH', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+    console.log(res);
+    return res;
+  }
+  render() {
+    return (
+      <div className="component-result">
+        <p className="title">ผลกรรม</p>
+        <p>{'คุณจะตายวันที่ ' + this.getDateString()}</p>
+      </div>
+    );
+  }
 }
 export default Result;
